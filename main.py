e5bec9e1180d10e5794cd58f913c6c3e4ef18c3a
ak4422920 <ak4422920@gmail.com>
2025-01-10 20:53:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 
 import pyrogram, os, asyncio
 
-app_id = int(os.environ.get("app_id", ""))
-api_hash = os.environ.get("api_hash", "")
+app_id = int(os.environ.get("app_id", "29171167"))
+api_hash = os.environ.get("api_hash", "7ea2149629e445936619f06a3c0dc716")
 bot_token = os.environ.get("bot_token", "")
 custom_caption = os.environ.get("custom_caption", "`{file_name}`\n\n**Powered By - @VJ_Botz**") # Here You Can Give Anything, if You Want Real File Name Then Use {file_name}
 
